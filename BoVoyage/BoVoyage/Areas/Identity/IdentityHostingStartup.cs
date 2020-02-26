69267483e4060ed87dde344c97c7845e8b3f34a5
Floups <fderosne@free.fr>
2020-02-26 12:18:07 +0100

    modif pour que ça fonctionne

---
+++ BoVoyage/BoVoyage/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BoVoyage.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BoVoyage.Areas.Identity.IdentityHostingStartup))]
+namespace BoVoyage.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

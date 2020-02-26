85a6bfa7cc3cb9812b2a60df82ca86e3eff94bc5
Dorian Guillaume <drnguillaume@gmail.com>
2020-02-26 14:46:30 +0100

    Mise en page accueil début

--- BoVoyage/BoVoyage/Controllers/HomeController.cs
+++ BoVoyage/BoVoyage/Controllers/HomeController.cs
@@ -6,20 +6,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BoVoyage.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BoVoyage.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BoVoyageContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BoVoyageContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+       
+        public async Task<IActionResult> Index()
         {
+            var voyagesMoinsCher = await _context.Voyage.OrderByDescending(v => v.PrixHt).Take(5).ToListAsync();
+            ViewBag.VoyagesMoinsCher = voyagesMoinsCher;
             return View();
         }
 

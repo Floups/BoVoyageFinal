f473e1a96afb293c6a898f11b57f0a91eef678e9
Floups <fderosne@free.fr>
2020-02-27 18:12:58 +0100

    attributs destinations

--- BoVoyage/BoVoyage/Models/Destination.cs
+++ BoVoyage/BoVoyage/Models/Destination.cs
@@ -12,13 +12,17 @@ namespace BoVoyage.Models
             Photo = new List<Photo>();
             Voyage = new HashSet<Voyage>();
         }
-
         public int Id { get; set; }
+        [Display(Name ="Pays")]
         public int? IdParente { get; set; }
+        [StringLength(100)]
         public string Nom { get; set; }
+        [Range(1,3)]
         public byte Niveau { get; set; }
+        [StringLength(1500)]
         public string Description { get; set; }
 
+        [Display(Name = "Pays")]
         public virtual Destination IdParenteNavigation { get; set; }
         public virtual ICollection<Destination> InverseIdParenteNavigation { get; set; }
         [DataType(DataType.ImageUrl)]
